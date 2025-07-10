75ed8b7808ffa85dc6eca5d0a8b42f0c4e43bd63
udehlee <ritzeepita@gmail.com>
2025-07-10 20:42:48 +0100

    setup main to initialize Hub, start server, and route WebSocket connections

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,22 @@
-package cmd
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/Udehlee/strim/internals"
+)
+
+func main() {
+	hub := internals.NewHub()
+	go hub.Run()
+
+	wsConn := internals.NewWsConn(hub)
+	mux := wsConn.Route()
+
+	log.Println("Starting WebSocket signaling server started on :8080")
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		log.Fatal("failed to start server")
+
+	}
+}
